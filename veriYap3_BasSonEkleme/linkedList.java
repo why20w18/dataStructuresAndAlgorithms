c34c5f67a6767ffdd86c5b16a015bf51672ecca2
why ? <147606593+why20w18@users.noreply.github.com>
2023-11-07 18:52:14 +0300

    Update linkedList.java

--- veriYap3_BasSonEkleme/linkedList.java
+++ veriYap3_BasSonEkleme/linkedList.java
@@ -1,1 +1,44 @@
 
+
+public class linkedList {
+    
+    node head = null;
+    node tail = null;
+    int cx;
+    
+    void basaEkle(int x){
+        //dugum olustur
+        node eleman = new node();
+        eleman.sayi = x;
+        if(head == null){
+            cx++;
+            eleman.next = head;
+            head = eleman;
+            tail = eleman;
+            System.out.println("liste olusturuldu ! basa eleman eklendi ! "+"dugum:"+cx);
+        }
+        else{
+            cx++;
+            eleman.next = head;
+            head = eleman;
+            System.out.println("\nbasa eleman eklendi ! "+"dugum:"+cx);
+        }
+        
+        
+    }
+    
+    void yazdir()
+    {
+        node temp = head;
+        
+        System.out.print("head-->");
+        while(temp != null){
+            
+            System.out.print(temp.sayi+"-->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+        
+    }    
+}
+
