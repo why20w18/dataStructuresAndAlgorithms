d62bcd12245be81b905959d02f88d87d64bf691a
why ? <147606593+why20w18@users.noreply.github.com>
2023-11-07 18:14:07 +0300

    Update main.java

--- veriYap1/main.java
+++ veriYap1/main.java
@@ -1,1 +1,21 @@
 
+
+public class main {
+
+    public static void main(String[] args){
+        
+        //düğüm oluşturacağım yani obje oluşturmalıyım
+        node A = new node();
+        node B = new node();
+        node C = new node();
+        //-->referansa obje oluşturmazsam düğüm olmaz çünkü düğümler ramde yer kaplar
+        
+        //düğümlere değer atayacağım benim düğümlerim isim ve yaş tutacak (String ve Integer)
+        
+        
+        
+        
+    }
+    
+}
+
