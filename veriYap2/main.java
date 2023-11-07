4d21575a4f513f7a3170f515228805c13667f655
why ? <147606593+why20w18@users.noreply.github.com>
2023-11-07 18:36:29 +0300

    Update main.java

--- veriYap2/main.java
+++ veriYap2/main.java
@@ -1,1 +1,19 @@
 
+
+public class main {
+    
+    public static void main(String[] args){
+        
+        linkedList listem = new linkedList();
+        
+        listem.nodeEkle(15, "ali");
+        listem.nodeEkle(16, "veli" );
+        listem.nodeEkle(17, "ahmet");
+        
+        listem.yazdir();
+        
+        
+        
+    }
+    
+}
