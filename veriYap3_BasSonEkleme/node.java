1c7232083a470cceacf1db16d17d89b2f997804e
why ? <147606593+why20w18@users.noreply.github.com>
2023-11-07 18:51:59 +0300

    Update node.java

--- veriYap3_BasSonEkleme/node.java
+++ veriYap3_BasSonEkleme/node.java
@@ -1,1 +1,8 @@
-//düğümlerin içereceği veritiplerini , head ve tail olusturmayi , sonraki dugumu gosterecek isaretcimizi , ekrana yazdirmada kullandigimiz tempi buradan tanimliyoruz
+
+
+public class node {
+    
+    int sayi;
+    node next;
+    
+}
