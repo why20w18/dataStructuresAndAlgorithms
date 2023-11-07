ef0df5c1cb99c6e2372a65a838c2abbeb01b217e
why ? <147606593+why20w18@users.noreply.github.com>
2023-11-07 20:09:48 +0300

    Update main.java

--- veriYap4_arayaElemanEkle/main.java
+++ veriYap4_arayaElemanEkle/main.java
@@ -5,16 +5,20 @@ public class main {
     public static void main(String[] args){
         
         linkedList listem = new linkedList();
+        
+        listem.arayaEkle(0, 8);
+        
         listem.sonaEkle(35);
         listem.basaEkle(15);
+        listem.arayaEkle(2, 44); // 15->8->35 iken benim listem 2.indexe ekledi o zamanki 2.index
+        
         listem.basaEkle(45);
         listem.sonaEkle(55);
         
-        
+        listem.arayaEkle(1, 11);
         listem.yazdir();
         
         
     }
     
 }
-
