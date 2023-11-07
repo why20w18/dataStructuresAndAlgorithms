cc8ecfd21d79e97500328c02e884ef4ac06a0924
why ? <147606593+why20w18@users.noreply.github.com>
2023-11-07 19:37:28 +0300

    Update main.java

--- veriYap4_arayaElemanEkle/main.java
+++ veriYap4_arayaElemanEkle/main.java
@@ -1,1 +1,20 @@
 
+
+public class main {
+    
+    public static void main(String[] args){
+        
+        linkedList listem = new linkedList();
+        listem.sonaEkle(35);
+        listem.basaEkle(15);
+        listem.basaEkle(45);
+        listem.sonaEkle(55);
+        
+        
+        listem.yazdir();
+        
+        
+    }
+    
+}
+
