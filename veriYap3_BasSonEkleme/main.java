55d8fcddb0fc9dceafd62d0093b50a0dfc9f1aa4
why ? <147606593+why20w18@users.noreply.github.com>
2023-11-07 18:51:36 +0300

    Update main.java

--- veriYap3_BasSonEkleme/main.java
+++ veriYap3_BasSonEkleme/main.java
@@ -1,1 +1,17 @@
 
+
+public class main {
+    
+    public static void main(String[] args){
+        
+        linkedList listem = new linkedList();
+        listem.basaEkle(10);
+        listem.basaEkle(15);
+        listem.basaEkle(20);
+        listem.yazdir();
+        
+    }
+    
+    
+}
+
