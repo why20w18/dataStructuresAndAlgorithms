9f244f2743cf9e8fea6fd4da6a16f6ce9f419e72
why ? <147606593+why20w18@users.noreply.github.com>
2023-11-08 21:33:38 +0300

    Update node.java

--- veriYap5_ciftYonlu/node.java
+++ veriYap5_ciftYonlu/node.java
@@ -2,12 +2,13 @@
 
 public class node{
     int data;
-    node next = null;
-    node prev = null;
+    node next;
+    node prev;
     //constructor sayesinde dugum olustururken argüman girmek artık zorunludur
     public node(int data){
         this.data = data;
-        
+        next = null;
+        prev = null;
     }
     
 }
