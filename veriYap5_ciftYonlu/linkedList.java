763deac1f9833b79476b2ca60a4eb0fee11a57e9
why ? <147606593+why20w18@users.noreply.github.com>
2023-11-08 18:55:10 +0300

    Update linkedList.java

--- veriYap5_ciftYonlu/linkedList.java
+++ veriYap5_ciftYonlu/linkedList.java
@@ -1,1 +1,21 @@
 
+
+public class linkedList{
+    
+    node head = null;
+    node tail = null;
+    
+    void basEkle(int data){
+        node eleman = new node(data);
+        
+        if(head == null){ //liste var mı yoksa olustur
+            head = eleman;
+            tail = eleman;
+        }
+        
+        
+    }
+    
+    
+    
+}
