71c0a2d6c411a08932b9048c4734d60234b53465
why ? <147606593+why20w18@users.noreply.github.com>
2023-11-08 18:55:38 +0300

    Update main.java

--- veriYap5_ciftYonlu/main.java
+++ veriYap5_ciftYonlu/main.java
@@ -1,1 +1,12 @@
+//cift yonlu linked list olusturuyoruz
+//basa , sona ve araya ekleme metodu icerecek
 
+public class main{
+    
+    public static void main(String[] args){
+     
+        
+        
+        
+    }
+}
