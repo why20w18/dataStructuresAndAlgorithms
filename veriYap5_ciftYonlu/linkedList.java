8ab2a231da6b0918060c6046e0bc7a06b76eb0a1
why ? <147606593+why20w18@users.noreply.github.com>
2023-11-08 19:00:24 +0300

    Update linkedList.java

--- veriYap5_ciftYonlu/linkedList.java
+++ veriYap5_ciftYonlu/linkedList.java
@@ -11,8 +11,30 @@ public class linkedList{
         if(head == null){ //liste var mı yoksa olustur
             head = eleman;
             tail = eleman;
+            System.out.println("liste olusturuldu : basEkle");
+        }
+        else{
+            //liste olusmussa ve basa ekleme yapacaksak
+            eleman.next = head;
+            head.prev = eleman;
+            head = eleman;
+            System.out.println("basa eleman eklendi ! : basEkle");
         }
         
+    }
+    //yazdir metoduyla yazdirabilmemizin sebebi next ile dugumleri birbirine baglamakti
+    //prev ile yazdirmak istiyorsak dugumleri yine birbirine baglamak zorundayiz
+    void duzYazdir(){
+        int cx = 0;
+        node temp = head;
+        //node temp2 = head; buna şimdilik gerek yok
+        
+        System.out.print("head->");
+        while(temp != null){
+            System.out.print(temp.data+"->");
+            temp = temp.next;
+        }
+        System.out.print("tail\n");
         
     }
     
