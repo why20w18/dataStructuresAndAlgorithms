47db1b5072b30a8d3f348d4275ba59d10837a04b
why ? <147606593+why20w18@users.noreply.github.com>
2023-11-13 14:46:20 +0300

    Create node.java

---
+++ veriYap6_Dairesel/daireselCiftYonlu/node.java
@@ -0,0 +1,15 @@
+
+
+public class node {
+    
+    int data;
+    node next;
+    node prev;
+    
+    public node(int data){
+        next = null;
+        prev = null;
+        this.data = data;
+    }
+    
+}
