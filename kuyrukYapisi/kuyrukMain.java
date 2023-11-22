e7376f958e170a5c9803aa99b7d8f32e22fd23f0
bug <147606593+why20w18@users.noreply.github.com>
2023-11-22 20:20:25 +0300

    Update kuyrukMain.java

--- kuyrukYapisi/kuyrukMain.java
+++ kuyrukYapisi/kuyrukMain.java
@@ -1,1 +1,20 @@
 
+
+public class kuyrukMain{
+    public static void main(String[] args) {
+        
+        kuyrukYapisi kuyruk1 = new kuyrukYapisi(5);
+        
+        kuyruk1.enQueue(10);
+        kuyruk1.enQueue(20);
+        kuyruk1.enQueue(30);
+        kuyruk1.deQueue();
+        
+        
+        kuyruk1.elemanSayisi();
+        kuyruk1.yazdir();
+        
+        
+    }
+    
+}
