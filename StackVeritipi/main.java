1e3aebd6ce296dd724336642f62b6029bd8145d4
why ? <147606593+why20w18@users.noreply.github.com>
2023-11-22 14:48:55 +0300

    Update main.java

--- StackVeritipi/main.java
+++ StackVeritipi/main.java
@@ -1,1 +1,20 @@
 
+
+public class main {
+    
+    public static void main(String[] args) {
+        StackYigin stack = new StackYigin(4);
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        System.out.println("pop() :"+stack.pop());
+        System.out.println("pop() :"+stack.pop());
+        System.out.println("pop() :"+stack.pop());
+        System.out.println("pop() :"+stack.pop());
+        System.out.println("pop() :"+stack.pop());
+    }
+    
+}
+
