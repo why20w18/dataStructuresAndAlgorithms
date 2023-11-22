c0a2f00d5313ed1b5431cf40a43592da8d190ff8
why ? <147606593+why20w18@users.noreply.github.com>
2023-11-22 14:49:15 +0300

    Create Stack.java

---
+++ StackVeritipi/Stack.java
@@ -0,0 +1,41 @@
+
+
+public class Stack {
+    
+    int dizi[]; 
+    int size;
+    int index;
+    
+    public StackYigin(int size){
+        this.size = size;
+        dizi = new int[size];
+        index = -1; //ekleme yapinca direkt 0.indexi gosterecek
+    }
+    
+    void push(int data){ //ekleme yapacak
+        if(isFull()){
+            System.out.println("stack doludur ! ekleme yapilamaz");
+        }
+        else{
+            index++;
+        dizi[index] = data;
+        System.out.println("push() :"+dizi[index]);
+        }
+    }
+    
+    int pop(){
+        if(isEmpty()){
+            System.out.println("stack bostur cikartma yapilamaz !");
+            return -1;
+        }
+        return dizi[index--];
+    }
+    boolean isFull(){
+        return index == (size-1); //boyut indexin 1 fazlası
+    }
+    
+    boolean isEmpty(){
+        return (index == -1);
+    }
+    
+}
